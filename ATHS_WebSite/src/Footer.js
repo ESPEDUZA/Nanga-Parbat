23b872bb27a4ae73533359e52699505745cda806
ESPEDUZA <eliott.germain@hotmail.fr>
2023-07-04 10:27:49 +0200

    [ADD] Footer

---
+++ ATHS_WebSite/src/Footer.js
@@ -0,0 +1,15 @@
+// Footer.js
+import React from 'react';
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footerContainer">
+                <p>&copy; 2023 Art Hunters. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
